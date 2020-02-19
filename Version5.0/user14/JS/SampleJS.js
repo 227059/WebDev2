f9699ade26369f4f96c9ed64b80742f7ac9e46c6
227059 <227059@shakopeeschools.org>
2020-02-19 14:07:47 -0600

    added stuff to the website

---
+++ Version5.0/user14/JS/SampleJS.js
@@ -0,0 +1,13 @@
+function changeText() {
+ document.getElementById("textChange").innerHTML="<div class='oranges' /div> :)";
+}
+
+function changeText2() {
+ document.getElementById("textChange").innerHTML="<div class='tylerthegoat' /div> :(";
+}
+
+function allCaps() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
+
